b91e31cbdca4b0a92b25d45014ab0f10c70e4dc5
Blooot <dalehamilton@trentu.ca>
2018-11-03 15:11:16 +0900

    fix style

--- src/components/Word.js
+++ src/components/Word.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Word = ({ kanji, kana, primary, secondary, pos  }) => {
     return (
-            <div className='inline-flex justify-around w-33 pa5'>
+            <div className='inline-flex justify-around w-33 pa5 flex-auto'>
             <div className=' f3 grow hide-child tc bg-light-blue-40 br4 shadow-4 mv3'>
                     <h1 className='white'>{kanji}</h1>
-                    <div className='mt0 f5 child bg-light-blue-40 white  fw3'>
+                    <div className='mt0 f5 child bg-light-blue-40 white fw3'>
                         <p>{kana}</p>
                         <p>{primary}</p>
                         <p>{secondary}</p>
